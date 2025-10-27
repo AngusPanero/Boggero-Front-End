d9642ce6abae25c692694fc92e1936b6185c72a2
Agustin Panero <agustinpanero@MacBook-Pro-de-Agustin.local>
2025-10-27 20:53:41 -0300

    FRONT BASE

---
+++ src/firebase/firebase.js
@@ -0,0 +1,16 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-app.js";
+
+import { getAuth } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-auth.js";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAzF63XCG_YdlD-RwTTiXQhvKp1RU-6acw",
+    authDomain: "boggero-7aab3.firebaseapp.com",
+    projectId: "boggero-7aab3",
+    storageBucket: "boggero-7aab3.firebasestorage.app",
+    messagingSenderId: "125636249323",
+    appId: "1:125636249323:web:a778f2bc897607d9b660ae",
+    measurementId: "G-LJ6YS66CGB"
+};
+
+export const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
